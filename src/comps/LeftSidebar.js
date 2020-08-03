8b5af0c93c019f28ce5bb5f3febf9cb19e86d8f4
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-03 16:46:08 +0100

    initial component planning

---
+++ src/comps/LeftSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSidebar =()=> (
+    <div className="LeftSideBar">
+        {/* logo */}
+        {/* menu */}
+        {/* socials */}
+    </div>
+);
+
+export default LeftSidebar
