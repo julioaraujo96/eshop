cd21c515eb3882e4d45600c6f76b7dfaf00a4324
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-05 16:11:48 +0100

    leftSideBar styling and assets

--- src/comps/LeftSideBar/LeftSidebar.js
+++ src/comps/LeftSideBar/LeftSidebar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../LeftSideBar/LeftSideBar.module.scss';
+import Logo from '../../assets/img/shoes.png';
+import menuSVG from '../../assets/img//open-menu.svg'
 
 const LeftSidebar =()=> (
-    <div className="LeftSideBar">
-        {/* logo */}
-        {/* menu */}
-        {/* socials */}
+    <div className={styles.LeftSideBar}>
+        <img src={Logo} alt='website logo' className={styles.logo}/>
+        <img src={menuSVG} alt='menu' className={styles.menu}/>
+        <p>Social <br></br>Networks</p>
     </div>
 );
 
