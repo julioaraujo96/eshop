9b9cb97038244deb2a9661708651f8780368a44a
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-07 00:16:29 +0100

    changed file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Helmet} from 'react-helmet'
-import LeftSideBar from './comps/LeftSideBar/LeftSidebar'
-import MainItem from './comps/MainItem/MainItem';
+import LeftSideBar from '../comps/LeftSideBar/LeftSidebar'
+import MainItem from '../comps/MainItem/MainItem';
+import styles from './App.module.scss'
 
 
 const App =()=>(
-  <div className="App">
+  <div className={styles.App}>
       <Helmet>
           <meta charSet='utf-8' />
           <title>Welcome to eShoes!</title>
