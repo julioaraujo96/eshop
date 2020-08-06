7e1975618e07f38599680526d1c6672de1455779
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-07 00:18:04 +0100

    coded and styles main item component (not cmplete)

--- src/comps/MainItem/MainItem.js
+++ src/comps/MainItem/MainItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from './MainItem.module.scss'
+import AirPresto from '../../assets/img/Collection/AirPresto.png';
 
 const MainItem =() =>(
-    <div>
-        it works
+    <div className={styles.MainItem}>
+        <p>$99</p>
+        <img src={AirPresto} alt='Nike Air Presto'/>
+        <p>
+            Summer 2020 <br></br> 
+            <strong>
+                Nike Air Presto
+            </strong>
+        </p>
     </div>
 );
 
