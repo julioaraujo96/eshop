ca3efac7770e5777095c2c59514303eebe257923
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-12 23:54:08 +0100

    connection to firestote and dinamic render mainItem

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Helmet} from 'react-helmet';
 import LeftSideBar from '../comps/LeftSideBar/LeftSidebar';
 import MainItem from '../comps/MainItem/MainItem';
 import Collection from '../comps/Collection/Collection';
+import db from '../firebase';
 import styles from './App.module.scss';
 
 
-const App =()=>(
-  <div className={styles.App}>
-      <Helmet>
-          <meta charSet='utf-8' />
-          <title>Welcome to eShoes!</title>
-        </Helmet>
-    <LeftSideBar/>
-    <MainItem/>   
-    <Collection/>
-  </div>
-);
+const App =()=>{
+  const [collection,setCollection] = useState([]);
+//useEffect is a piece of code that runs based on a condition (empty array, executes only once when it loads, with dependencie, executes once and when the variable inside the array changes)
+useEffect(()=>{
+  db.collection('shoes').onSnapshot(snapshot =>(
+    setCollection(snapshot.docs.map(doc => doc.data()))
+  ))
+},[]);
+
+let mainItem = null;
+  if (collection.length > 0) {
+    const mainShoe = collection.filter(c => c.mainItem === true);
+    mainItem =(
+      <MainItem 
+        name={mainShoe[0].name} 
+        season={mainShoe[0].season} 
+        price={mainShoe[0].price}
+        url={mainShoe[0].url}
+      />
+    );
+  }
+  return(
+    <div className={styles.App}>
+        <Helmet>
+            <meta charSet='utf-8' />
+            <title>Welcome to eShoes!</title>
+          </Helmet>
+      <LeftSideBar/>
+      {mainItem}
+      <Collection/> 
+    </div>
+)};
 
 export default App;
