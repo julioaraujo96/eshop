133e44a2f3b9dff9e54839774c3f9ca3ae8ad33b
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-13 22:10:50 +0100

    importing materialui icons and styling

--- src/comps/RightSidebar/RightSidebar.js
+++ src/comps/RightSidebar/RightSidebar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './RightSidebar.module.scss';
-import Logo from '../../assets/img/shoes.png';
-import menuSVG from '../../assets/img//open-menu.svg'
+import SettingsIcon from '@material-ui/icons/Settings';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const RightSidebar =()=> (
     <div className={styles.RightSidebar}>
-       
+        <SettingsIcon/>
+        <ShoppingCartOutlinedIcon/>
+        <h2>Cart is empty</h2>
     </div>
 );
 
