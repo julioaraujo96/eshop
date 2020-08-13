1071daa05b0af754935d81efefefada35fbc3782
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-13 17:33:40 +0100

    started working on rightsidebar

---
+++ src/comps/RightSidebar/RightSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RightSidebar.module.scss';
+import Logo from '../../assets/img/shoes.png';
+import menuSVG from '../../assets/img//open-menu.svg'
+
+const RightSidebar =()=> (
+    <div className={styles.RightSidebar}>
+       
+    </div>
+);
+
+export default RightSidebar;
