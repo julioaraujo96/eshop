82cc244fe627dac6af65794a803c31334e01bff8
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-13 18:17:42 +0100

    Applying iconbutton ui to leftsidebar

--- src/comps/LeftSideBar/LeftSidebar.js
+++ src/comps/LeftSideBar/LeftSidebar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './LeftSideBar.module.scss';
 import Logo from '../../assets/img/shoes.png';
+import { IconButton } from '@material-ui/core';
 import menuSVG from '../../assets/img//open-menu.svg'
 
 const LeftSidebar =()=> (
     <div className={styles.LeftSideBar}>
-        <img src={Logo} alt='website logo' className={styles.logo}/>
-        <img src={menuSVG} alt='menu' className={styles.menu}/>
+        <IconButton>
+            <img src={Logo} alt='website logo' className={styles.logo}/>
+        </IconButton>
+        <IconButton>
+            <img src={menuSVG} alt='menu' className={styles.menu}/>
+        </IconButton>
         <p>Social <br></br>Networks</p>
     </div>
 );
