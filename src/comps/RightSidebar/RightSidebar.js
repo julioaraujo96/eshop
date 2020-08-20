0b24501482aa177457f76ee9f9535a4958dc09de
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-08-21 00:16:45 +0100

    applying material ui to the icons

--- src/comps/RightSidebar/RightSidebar.js
+++ src/comps/RightSidebar/RightSidebar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './RightSidebar.module.scss';
+import { IconButton } from '@material-ui/core';
 import SettingsIcon from '@material-ui/icons/Settings';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const RightSidebar =()=> (
     <div className={styles.RightSidebar}>
-        <SettingsIcon fontSize="large"/>
-        <ShoppingCartOutlinedIcon fontSize="large"/>
+        <IconButton>
+            <SettingsIcon fontSize="large"/>
+        </IconButton>
+        <IconButton>
+            <ShoppingCartOutlinedIcon fontSize="large"/>
+        </IconButton>
         <h2>Cart is empty</h2>
     </div>
 );
