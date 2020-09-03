6b9c5bff21c94e91aaf58b5d2f01532c8438d4fa
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-03 18:36:31 +0100

    styled component with material ui and css effects

--- src/comps/Pagination/Pagination.js
+++ src/comps/Pagination/Pagination.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ArrowRight from '@material-ui/icons/ArrowRightAlt';
+import { IconButton } from '@material-ui/core';
 import styles from './Pagination.module.scss'
 
 
 const Pagination = props =>(
     <div className={styles.Pagination}>
-        <h1>i work</h1>
+        <IconButton>
+            <ArrowRight fontSize="Large" className={[styles.icons, styles.rotate].join(' ')}/>
+        </IconButton>
+        <div className={styles.centerContainer}>
+            <h2>1</h2>
+            <div className={styles.divider}></div>
+            <h1>3</h1>
+        </div>
+        <IconButton>
+            <ArrowRight fontSize="Large" className={styles.icons}/>
+        </IconButton>
     </div>
 );
 
