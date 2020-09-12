2953520a60e1e1a151c09eef711dfca9c0fc875d
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-12 21:47:45 +0100

    animated social icons

--- src/comps/LeftSideBar/LeftSidebar.js
+++ src/comps/LeftSideBar/LeftSidebar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './LeftSideBar.module.scss';
 import Logo from '../../assets/img/shoes.png';
-import { IconButton } from '@material-ui/core';
 import menuSVG from '../../assets/img//open-menu.svg'
+import { IconButton } from '@material-ui/core';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 const LeftSidebar =()=> (
     <div className={styles.LeftSideBar}>
@@ -12,8 +17,22 @@ const LeftSidebar =()=> (
         <IconButton>
             <img src={menuSVG} alt='menu' className={styles.menu}/>
         </IconButton>
-        <p>Social <br></br>Networks</p>
-    </div>
+        <div>
+            <div className={styles.socialsContainer}>
+                <div className={styles.textHide}>
+                    <p>Social <br></br>Networks</p>
+                    <div className={styles.socialIcons}>
+                        <GitHubIcon fontSize="large" className={styles.icons}/>
+                        <InstagramIcon fontSize="large" className={styles.icons}/>
+                        <TwitterIcon fontSize="large" className={styles.icons}/>
+                        <FacebookIcon fontSize="large" className={styles.icons}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+  		
+  
 );
 
 export default LeftSidebar
