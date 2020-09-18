a17cf9430dfde4807e727c981d39b17501e78253
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-18 17:07:03 +0100

    animated item container

--- src/comps/Collection/CollectionItem/CollectionItem.js
+++ src/comps/Collection/CollectionItem/CollectionItem.js
@@ -6,8 +6,10 @@ const CollectionItem = props =>(
         <div className={styles.priceCircle}>
             <h1>${props.price}</h1>
         </div>
-        <img src={props.url} alt={props.name}/>
-        <h2>{props.name}</h2>
+        <div className={styles.imageContainer}> 
+            <img src={props.url} alt={props.name}/>
+        </div>
+        <div className={styles.title}>{props.name}</div>
     </div>
 );
 
