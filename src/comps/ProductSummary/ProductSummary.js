27e368568247d232bfd903801b8455103780c020
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-27 00:14:52 +0100

    initialize productsummary component

--- src/comps/ProductSummary/ProductSummary.js
+++ src/comps/ProductSummary/ProductSummary.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './ProductSummary.module.scss';
 
-const ProductSummary = (props) =>
-(
-    <div>
-
-    </div>
-);
+const ProductSummary = (currentSummary,setShowModal) =>{
+    console.log(currentSummary);
+    return(
+        <div>
+            <p>works</p>
+        </div>
+    )
+}
 
 export default ProductSummary;
