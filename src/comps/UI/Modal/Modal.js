30e1c9e9bb894acaac52dcaae08309cd3e9e508c
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-28 00:28:42 +0100

    added a backdrop for the modal, starting to add framer

--- src/comps/UI/Modal/Modal.js
+++ src/comps/UI/Modal/Modal.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import {motion,AnimatePresence} from 'framer-motion';
 import styles from './Modal.module.scss';
+
+const backdrop ={
+    visible:{opacity:1},
+    hidden:{opacity:0}
+}
 
 const Modal = ({showModal,children,setShowModal}) =>{
     return(
-        <div>
-             {showModal && (
-                <div className={styles.Modal}>
-                   { children}
-                </div>
+        <AnimatePresence exitBeforeEnter>
+            {showModal && (
+                <motion.div className={styles.backdrop}
+                variants={backdrop}
+                initial="hidden"
+                animate="visible">
+                    
+                </motion.div>
             )}
-        </div>
+        </AnimatePresence>
     )
 }
 
