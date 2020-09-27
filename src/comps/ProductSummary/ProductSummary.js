6e0463ec4c1976f04befacafc13b75c4b5b4cbac
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-09-27 21:14:55 +0100

    product summary html

--- src/comps/ProductSummary/ProductSummary.js
+++ src/comps/ProductSummary/ProductSummary.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './ProductSummary.module.scss';
 
+
 const ProductSummary = (currentSummary,setShowModal) =>{
     console.log(currentSummary);
     return(
-        <div>
-            <p>works</p>
-        </div>
+        <article>
+            <div>
+                <h3>Men's Shoe</h3>
+                <h3>{currentSummary.price}</h3>
+            </div>
+            <div>
+                <img src={currentSummary.url} alt={currentSummary.name} />
+            </div>
+            <div>
+                <p>Select Size</p>
+            </div>
+            <div>
+                <button>Add to cart</button>
+                <button>Checkout</button>
+            </div>
+        </article>
     )
 }
 
