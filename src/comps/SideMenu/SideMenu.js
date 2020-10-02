2a393439e94fff6a2002392bef8c1d62a032d15b
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-10-02 22:13:01 +0100

    initialize Side menu component

---
+++ src/comps/SideMenu/SideMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {motion,AnimatePresence} from 'framer-motion';
+import styles from './SideMenu.module.scss'
+
+
+const SideMenu = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+export default SideMenu;
