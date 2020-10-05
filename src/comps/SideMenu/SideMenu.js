727429e3ba253802ae33988e8e1787bc2b9f2d40
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-10-05 02:29:44 +0100

    added styling, anim bg shoe and social icons

--- src/comps/SideMenu/SideMenu.js
+++ src/comps/SideMenu/SideMenu.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import {motion,AnimatePresence} from 'framer-motion';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Logo from '../../assets/img/shoes.png';
+import Shoe from '../../assets/img/pegasus.png';
 import styles from './SideMenu.module.scss'
 
 
@@ -21,6 +27,37 @@ const sideMenu={
         }
 }
 
+const backgroundShoe ={
+    hidden:{
+        right:"-100px",
+        opacity:0
+    },
+    visible:{
+        right:"-50px",
+        opacity:1,
+        transition:{
+            duration:0.8
+        }
+    }
+}
+
+const socialIcons ={
+    hidden:{
+        x:"-100vw",
+        opacity:0
+    },
+    visible:{
+        x:0,
+        opacity:1,
+        scale:1.2,
+        transition:{
+            duration:1.5,
+            type:'spring',
+            stiffness:120
+        }
+    }
+}
+
 const SideMenu = ({showMenu}) => {
     return (
         <AnimatePresence exitBeforeEnter>
@@ -38,7 +75,34 @@ const SideMenu = ({showMenu}) => {
                          exit="hidden"
                          className={styles.SideMenu}
                          >
- 
+                                <img src={Logo} alt='website logo' className={styles.logo}/>
+                             <motion.img 
+                             src={Shoe} 
+                             alt='Nike Pegasus Blue' 
+                             className={styles.backgroundShoe}
+                             variants={backgroundShoe}
+                             initial="hidden"
+                             animate="visible"
+                             />
+                             <ul>
+                                 <li>About</li>
+                                 <li>Sneakers</li>
+                                 <li>Acessories</li>
+                                 <li>Stores</li>
+                                 <li>Contacts</li>
+                             </ul>
+
+                             <motion.div className={styles.socialIcons}
+                             variants={socialIcons}
+                             initial="hidden"
+                             animate="visible"
+                             >
+                                <GitHubIcon fontSize="large" className={styles.icons}/>
+                                <InstagramIcon fontSize="large" className={styles.icons}/>
+                                <TwitterIcon fontSize="large" className={styles.icons}/>
+                                <FacebookIcon fontSize="large" className={styles.icons}/>
+                                
+                             </motion.div>
                      </motion.div>
              </motion.div>
             )}
