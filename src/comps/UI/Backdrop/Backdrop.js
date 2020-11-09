5237694d3f499f5e5de583322eddf7c6352bcad5
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-11-09 22:44:30 +0000

    backdrop component creation

---
+++ src/comps/UI/Backdrop/Backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '.../Backdrop.module.scss';
+
+const Backdrop = (props) => {
+    return (
+        <div className={styles.Backdrop}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Backdrop;
